11aaa5757d441ad2961f175a34697a4906e80b4b
Thikshana Ekanayake <110461079+Thikshana-Ekanayake@users.noreply.github.com>
2024-07-08 18:09:21 +0530

    Routing Done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,45 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Register from "./pages/Regsiter.jsx";
+import Home from "./pages/Home.jsx";
+import Single from "./pages/Single.jsx";
+import Login from "./pages/Login.jsx";
+import Write from "./pages/Write.jsx";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+
+  {
+    path: "/Register",
+    element: <Register/>,
+  },
+
+  {
+    path: "/Single",
+    element: <Single/>,
+  },
+
+  {
+    path: "/Login",
+    element: <Login/>,
+  },
+
+  {
+    path: "/write",
+    element: <write/>,
+  },
+]);
+
 function App() {
-  return <div>Hello World!</div>;
+  return <div>
+    <RouterProvider router={router} />
+  </div>;
 }
 
 export default App;
