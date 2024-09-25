6d33a34abf767d2e0e4f62ec35679c46411781ee
Iruka Pathirana <thuthinidhijpic.20@uom.lk>
2024-09-25 16:50:25 +0530

    backend done

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,6 +1,18 @@
-import express from 'express';
+import express from "express";
+import {
+  addPost,
+  deletePost,
+  getPost,
+  getPosts,
+  updatePost,
+} from "../controllers/post.js";
 
-const router =express.Router();
-router.get('/test', (req, res) => {
-    res.json('this is the post');
-})
+const router = express.Router();
+
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.post("/", addPost);
+router.delete("/:id", deletePost);
+router.put("/:id", updatePost);
+
+export default router;
